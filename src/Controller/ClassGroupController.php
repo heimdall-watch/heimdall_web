5607f7d8fc6f508e5eb37222205da83fb503c1f7
Samyhz <bouleoud.samyh@hotmail.fr>
2019-04-19 11:02:32 +0200

    Ajout du CRUD ClassGroup
    Modification des fixtures pour créer une présence et un groupe

---
+++ src/Controller/ClassGroupController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ClassGroup;
+use App\Form\ClassGroupType;
+use App\Repository\GroupRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/class")
+ */
+class ClassGroupController extends AbstractController
+{
+    /**
+     * @Route("/", name="class_group_index", methods={"GET"})
+     */
+    public function index(GroupRepository $groupRepository): Response
+    {
+        return $this->render('class_group/index.html.twig', [
+            'class_groups' => $groupRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="class_group_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $classGroup = new ClassGroup();
+        $form = $this->createForm(ClassGroupType::class, $classGroup);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($classGroup);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('class_group_index');
+        }
+
+        return $this->render('class_group/new.html.twig', [
+            'class_group' => $classGroup,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="class_group_show", methods={"GET"})
+     */
+    public function show(ClassGroup $classGroup): Response
+    {
+        return $this->render('class_group/show.html.twig', [
+            'class_group' => $classGroup,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="class_group_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, ClassGroup $classGroup): Response
+    {
+        $form = $this->createForm(ClassGroupType::class, $classGroup);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('class_group_index', [
+                'id' => $classGroup->getId(),
+            ]);
+        }
+
+        return $this->render('class_group/edit.html.twig', [
+            'class_group' => $classGroup,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="class_group_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, ClassGroup $classGroup): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$classGroup->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($classGroup);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('class_group_index');
+    }
+}
--- src/DataFixtures/DevFixtures.php
+++ src/DataFixtures/DevFixtures.php
@@ -4,7 +4,10 @@ namespace App\DataFixtures;
 
 use App\Entity\Admin;
 use App\Entity\Auth\Client;
+use App\Entity\ClassGroup;
+use App\Entity\RollCall;
 use App\Entity\Student;
+use App\Entity\StudentPresence;
 use App\Entity\Teacher;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -45,6 +48,27 @@ class DevFixtures extends Fixture
                 ->setEmail("sosthen.gaillard@gmail.com")
                 ->setPassword($this->encoder->encodePassword($jfpp, "jfpp"));
         $manager->persist($jfpp);
+
+        // Group M2 APP
+        $group = new ClassGroup();
+        $group->setName("M2 APP");
+        $manager->persist($group);
+
+        // Rollcall 1
+        $date = new \DateTime();
+        $rollcallTest = new RollCall();
+        $rollcallTest->setClassGroup($group)
+                    ->setTeacher($jfpp)
+                    ->setDate($date)
+                    ->setDuration(1);
+        $manager->persist($rollcallTest);
+
+        // Student Presence 1
+        $presenceTest = new StudentPresence();
+        $presenceTest->setStudent($sosthen)
+                    ->setPresent(true)
+                    ->setRollcall($rollcallTest);
+        $manager->persist($presenceTest);
 
         $manager->flush();
     }
