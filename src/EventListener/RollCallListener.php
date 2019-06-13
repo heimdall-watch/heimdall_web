04165236d584a1ff140ced540186992bf785f4f4
SosthenG <pro@sosthen.fr>
2019-06-13 13:40:35 +0200

    Fix no onesignal

--- src/EventListener/RollCallListener.php
+++ src/EventListener/RollCallListener.php
@@ -21,18 +21,22 @@ class RollCallListener
 
     public function postPersist(RollCall $rollCall)
     {
-        foreach ($rollCall->getStudentPresences() as $studentPresence) {
-            if ($studentPresence->getPresent() === false || $studentPresence->getLate() !== null) {
-                $notif = [
-                    'contents' => [
-                        'en' => 'You are ' . ($studentPresence->getPresent() === false ? 'absent' : 'late') . ' to the class of ' . $rollCall->getTeacher()->getFirstname() . ' ' . $rollCall->getTeacher()->getLastName(),
-                        'fr' => 'Vous êtes ' . ($studentPresence->getPresent() === false ? 'absent' : 'en retard') . ' au cours de ' . $rollCall->getTeacher()->getFirstname() . ' ' . $rollCall->getTeacher()->getLastName(),
-                    ],
-                    'include_player_ids' => $studentPresence->getStudent()->getDevices()
-                ];
-                $this->api->notifications->add($notif);
-                $this->logger->debug("Sending onesignal notif to " . $studentPresence->getStudent()->getUsername() . ' : ' . json_encode($notif));
+        try {
+            foreach ($rollCall->getStudentPresences() as $studentPresence) {
+                if ($studentPresence->getPresent() === false || $studentPresence->getLate() !== null && !empty($studentPresence->getStudent()->getDevices())) {
+                    $notif = [
+                        'contents' => [
+                            'en' => 'You are ' . ($studentPresence->getPresent() === false ? 'absent' : 'late') . ' to the class of ' . $rollCall->getTeacher()->getFirstname() . ' ' . $rollCall->getTeacher()->getLastName(),
+                            'fr' => 'Vous êtes ' . ($studentPresence->getPresent() === false ? 'absent' : 'en retard') . ' au cours de ' . $rollCall->getTeacher()->getFirstname() . ' ' . $rollCall->getTeacher()->getLastName(),
+                        ],
+                        'include_player_ids' => $studentPresence->getStudent()->getDevices()
+                    ];
+                    $this->api->notifications->add($notif);
+                    $this->logger->debug("Sending onesignal notif to " . $studentPresence->getStudent()->getUsername() . ' : ' . json_encode($notif));
+                }
             }
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
         }
     }
 }
