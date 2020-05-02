2ef91c0f7896dc554a7c06fb148dd8fb41be1021
massong41 <35001438@parisnanterre.fr>
2020-05-03 01:05:28 +0200

    wip

--- src/Controller/LessonController.php
+++ src/Controller/LessonController.php
@@ -4,11 +4,15 @@ namespace App\Controller;
 
 use App\Entity\Student;
 use App\Entity\Lesson;
-use App\Form\StudentImportType;
+use App\Entity\Presence;
+use App\Entity\StudentPresence;
+use App\Form\PresenceType;
 use App\Form\LessonImportType;
 use App\Form\LessonType;
 use App\Repository\LessonRepository;
 use App\Repository\TeacherRepository;
+use App\Repository\StudentRepository;
+use App\Repository\StudentPresenceRepository;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Knp\Component\Pager\PaginatorInterface;
 use Port\Csv\CsvReader;
@@ -21,6 +25,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ODM\PHPCR\Query\Query;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
 
 /**
  * @Route("/lesson")
@@ -62,32 +68,6 @@ class LessonController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="lesson_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $lesson = new Lesson();
-        $form = $this->createForm(LessonType::class, $lesson);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $lesson->setPlainPassword($lesson->getPassword());
-
-            $entityManager->persist($lesson);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('lesson_index');
-        }
-
-        return $this->render('lesson/form.html.twig', [
-            'lesson' => $lesson,
-            'form' => $form->createView(),
-            'update' => false,
-        ]);
-    }
-
-    /**
      * @Route("/{id}", name="lesson_show", methods={"GET"})
      */
     public function show(Lesson $lesson): Response
@@ -97,28 +77,55 @@ class LessonController extends AbstractController
         ]);
     }
     
-
     /**
-     * @Route("/{id}/edit", name="lesson_edit", methods={"GET","POST"})
+     * @Route("/new", name="admin_new", methods={"GET","POST"})
      */
-    public function edit(Request $request, Lesson $lesson): Response
+    public function new(Request $request, StudentRepository $sr, StudentPresenceRepository $spr,LessonRepository $lr): Response
     {
-        $form = $this->createForm(LessonType::class, $lesson);
+        $presence = new Presence();
+        $form = $this->createForm(PresenceType::class, $presence);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+            $entityManager = $this->getDoctrine()->getManager();
+            $sp = new StudentPresence();
+            $listSP = $spr->getFindAllQuery()->getResult();
+            foreach($listSP as $stdPre){
+                if($stdPre->getStudent()->getId() == $presence->getIdStudent() 
+                    && $stdPre->getlesson()->getId() == $presence->getIdLesson()){
+                    $sp = $stdPre;
+                }
+            }
+            $listLesson = $lr->getFindAllQuery()->getResult();
+            foreach($listLesson as $les){
+                if($les->getId() == $presence->getIdLesson()){
+                    $sp->setlesson($les);
+                    if($presence->getLate() > 0){
+                        $dt = new DateInterval("PT".$presence->getLate()."M");
+                        $dateLate = $les->setDateStart()->add($dt);
+                        $sp->setLate($dateLate);
+                    }
+                    break;
+                }
+            }
+            $listStudent = $sr->getFindAllQuery()->getResult();
+            foreach($listStudent as $s){
+                if($s->getId() == $presence->getIdStudent()){
+                    $sp->setStudent($s);
+                    break;
+                }
+            }
+            if($presence->getPresent() == "true"){
+                $sp->setPresent(true);
+            } else {
+                $sp->setPresent(false);
+            }
+            $entityManager->persist($sp);
+            $entityManager->flush();
 
-            return $this->redirectToRoute('lesson_index', [
-                'id' => $lesson->getId(),
-            ]);
         }
 
-        return $this->render('lesson/form.html.twig', [
-            'lesson' => $lesson,
-            'form' => $form->createView(),
-            'update' => true,
-        ]);
+        //return $this->redirectToRoute('lesson_index');
     }
 
 }
