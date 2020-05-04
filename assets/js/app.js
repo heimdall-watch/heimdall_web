135702cb8072782ddaeab53fd3d7834fe4f507f6
bas.mauvieux@gmail.com <bas.mauvieux@gmail.com>
2020-05-04 17:40:08 +0200

    add css working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,4 @@
 import '../css/app.scss';
-
 import $ from 'jquery';
 import 'bootstrap';
 
@@ -9,5 +8,14 @@ import 'bootstrap-datepicker';
 
 import 'select2';
 
+import test from './student_homepage'
+
 window.$ = $;
 
+$('.input-group-btn').on('click', function () {
+    alert("test");
+});
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
